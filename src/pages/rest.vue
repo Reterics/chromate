90535924dca0b2477774020673d9b65133200920
Attila Reterics <reterics.attila@gmail.com>
2024-07-05 21:43:56 +0200

    Rest api frontend

--- src/pages/rest.vue
+++ src/pages/rest.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="api-tester">
-    <h1>REST API Tester</h1>
     <form @submit.prevent="testApi">
-      <div>
-        <label for="url">URL:</label>
-        <input type="text" v-model="url" id="url" required />
+      <div class="grid grid-cols-2 gap-6 pt-2">
+        <div class="relative z-0 w-full mb-3 group">
+          <label for="url" class="block mb-2 text-sm font-medium text-gray-900">URL:</label>
+          <input type="text" id="url" v-model="url" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://127.0.0.1/" required />
+        </div>
+        <div class="relative z-0 w-full mb-3 group">
+          <label for="method" class="block mb-2 text-sm font-medium text-gray-900">Method:</label>
+          <select v-model="method"  id="method" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="GET">GET</option>
+            <option value="POST">POST</option>
+            <option value="PUT">PUT</option>
+            <option value="DELETE">DELETE</option>
+          </select>
+
+        </div>
+      </div>
+
+      <div class="relative z-0 w-full mb-3 group">
+        <label for="headers" class="block mb-2 text-sm font-medium text-gray-900">Headers (JSON format):</label>
+        <textarea v-model="headers" id="headers" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+      </div>
+      <div class="relative z-0 w-full mb-3 group">
+        <label for="body" class="block mb-2 text-sm font-medium text-gray-900">Body:</label>
+        <textarea v-model="body" id="body" :disabled="method === 'GET'" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
       </div>
       <div>
-        <label for="method">Method:</label>
-        <select v-model="method" id="method">
-          <option>GET</option>
-          <option>POST</option>
-          <option>PUT</option>
-          <option>DELETE</option>
-        </select>
+        <button type="submit" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Test API</button>
       </div>
-      <div>
-        <label for="headers">Headers (JSON format):</label>
-        <textarea v-model="headers" id="headers"></textarea>
-      </div>
-      <div>
-        <label for="body">Body:</label>
-        <textarea v-model="body" id="body" :disabled="method === 'GET'"></textarea>
-      </div>
-      <button type="submit">Test API</button>
     </form>
     <div v-if="response">
       <h2>Response</h2>
@@ -67,39 +72,12 @@ export default {
 .api-tester {
   max-width: 600px;
   margin: 0 auto;
-  padding: 20px;
 }
-.api-tester h1 {
-  text-align: center;
-}
-.api-tester form {
-  display: flex;
-  flex-direction: column;
-}
-.api-tester form div {
-  margin-bottom: 10px;
-}
-.api-tester form label {
-  font-weight: bold;
-}
-.api-tester form input,
-.api-tester form select,
-.api-tester form textarea {
+
+.api-tester button[type=submit] {
   width: 100%;
-  padding: 8px;
-  margin-top: 5px;
-  box-sizing: border-box;
 }
-.api-tester form button {
-  padding: 10px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  cursor: pointer;
-}
-.api-tester form button:hover {
-  background-color: #0056b3;
-}
+
 .api-tester pre {
   background-color: #f8f9fa;
   padding: 10px;
